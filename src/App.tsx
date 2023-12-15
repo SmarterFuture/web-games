af5291153897e765bd47d72c5d17e287386d0f0b
SmarterFuture <anonymous.daisy.007@gmail.com>
2023-12-15 23:40:18 +0100

    react app - tic tac toe w/ bug of not clearing tiles in resize

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React, {MouseEventHandler, useState} from "react";
+import Slider from '@mui/material/Slider';
+
+interface Button {
+    value: string,
+    onClick: MouseEventHandler<HTMLButtonElement>
+}
+
+interface Size {
+    side: number
+}
+
+
+export function Square({value, onClick}: Button) {
+
+    return (
+        <button className="square" onClick={onClick}>
+            { value }
+        </button>
+    );
+}
+
+export function Game({side}: Size) {
+    
+    const [xNext, setXNext] = useState<boolean>(true);
+    const [tiles, setTiles] = useState<Array<string>>(Array(side ^ 2).fill(""));
+
+    function playerTurn( index: number ) {
+        const nextState = tiles.slice();
+        const symbol = xNext ? "X" : "O";
+        nextState[index] = symbol;
+        setTiles(nextState);
+        setXNext(!xNext);
+    }
+
+    let rows: Array<React.JSX.Element> = [];
+    for (let y = 0; y < side; y++) {
+        let cols: Array<React.JSX.Element> = [];
+        for (let x = 0; x < side; x++) {
+            const pos = y * side + x
+            cols.push(
+                <Square key={x} value={tiles[pos]} onClick={() => playerTurn(pos)}/>
+            );
+        }
+        rows.push(<div key={y} className="board-row">{cols}</div>);
+    } 
+    return (
+        <>
+            {rows}
+        </>
+    );
+}
+
+export default function Frame() {
+    const [size, setSize] = useState<number>(3);
+    function handleChange(_: Event, newValue: number | number[]) {
+        setSize(newValue as number);
+    }
+    
+    return (
+        <>
+            <Slider min={3} max={10} onChange={handleChange}/>
+            <Game side={size} />
+        </>
+    )
+}
+
